a4b33a4ba1530e95c2312d7abc820c142e40f6be
PascalCr <64025214+PascalCr@users.noreply.github.com>
2020-06-11 12:29:01 +0200

    initial

--- src/FiniteElements.jl
+++ src/FiniteElements.jl
@@ -1,5 +1,10 @@
 module FiniteElements
 
 # Write your package code here.
+export Abc
+
+struct Abc
+    hallo::Float64
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using FiniteElements
 using Test
 
 @testset "FiniteElements.jl" begin
-    # Write your tests here.
+    a = Abc(1.2)
+    @test a.hallo == 1.2
+end
+@testset "FiniteElements2" begin
+    a = Abc(1.2)
+    @test_broken a.hallo == 1.3
 end
